959e238e64956a6d8eeacc865e9f067c3522f302
Alexey917 <pochtovii.1996@mail.ru>
2025-06-15 18:46:23 +0300

    Сделал компонент с Support и добавил адаптивность

---
+++ my-app/src/components/Support/Support.tsx
@@ -0,0 +1,78 @@
+import { FC, useState } from 'react';
+import { Button } from '../UI/Button/Button';
+
+import classes from './Support.module.scss';
+import sprite from '../../assets/sprite.svg';
+
+//^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)
+
+export const Support: FC = () => {
+  const [value, setValue] = useState<string>('');
+  const [errorLabel, setErrorLabel] = useState<string>('');
+  const [canSend, setCanSend] = useState<boolean>(true);
+
+  const emailValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isValid =
+      /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)/;
+    setValue(e.target.value);
+    if (!isValid.test(value)) {
+      setErrorLabel('Некорректный e-mail');
+      setCanSend(true);
+    } else {
+      setErrorLabel('');
+      setCanSend(false);
+    }
+  };
+
+  return (
+    <section className={classes.support} aria-labelledby="support-heading">
+      <h4 className={classes.support__title} id="support-heading">
+        Support
+      </h4>
+      <p className={classes.support__text}>
+        Subscribe Newsletter & get
+        <br />
+        <span className={classes.support__textSpan}>Bank News</span>
+      </p>
+      <form action="#" noValidate name="subscribe">
+        <fieldset
+          className={classes.support__fieldset}
+          aria-describedby="email-hint"
+        >
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            aria-required="true"
+            placeholder=" "
+            className={classes.support__input}
+            onChange={emailValidation}
+          />
+          <label htmlFor="email" className={classes.support__label}>
+            <svg className={classes.support__emailIcon} aria-hidden="true">
+              <use href={`${sprite}#email`}></use>
+            </svg>
+            <span className={classes.support__emailPlaceholder}>
+              Your email
+            </span>
+          </label>
+          <label htmlFor="email" className={classes.support__errorLabel}>
+            {errorLabel}
+          </label>
+          <Button
+            type="submit"
+            className={classes.support__btn}
+            aria-label="Subscribe to newsletter"
+            disabled={canSend}
+          >
+            <svg className={classes.support__letterIcon} aria-hidden="true">
+              <use href={`${sprite}#letter`}></use>
+            </svg>
+            <span className={classes.button_text}>Subscribe</span>
+          </Button>
+        </fieldset>
+      </form>
+    </section>
+  );
+};
--- my-app/src/pages/HomePage/HomePage.tsx
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import { Features } from '../../components/Features/Features';
 import { Anywhere } from '../../components/Anywhere/Anywhere';
 import { Currencies } from '../../components/Currencies/Currencies';
 import { ChooseCard } from '../../components/ChooseCard/ChooseCard';
+import { Support } from '../../components/Support/Support';
 
 import classes from './HomePage.module.scss';
 
@@ -11,8 +12,9 @@ export const HomePage: FC = () => {
     <main className={classes.main}>
       <ChooseCard />
       <Features />
-      {/* <Currencies /> */}
+      <Currencies />
       <Anywhere />
+      <Support />
     </main>
   );
 };
